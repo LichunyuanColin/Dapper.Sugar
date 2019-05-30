afbda160772b8040189e8741ce4767d5bc4ff4fd
李春园 <1187834837@qq.com>
2019-05-30 14:15:51 +0800

    添加单元测试

---
+++ UnitTest/GenerateDataBase.sql
@@ -0,0 +1,59 @@
+-- MySQL dump 10.13  Distrib 5.7.17, for Win64 (x86_64)
+--
+-- Host: 10.0.0.2    Database: test
+-- ------------------------------------------------------
+-- Server version	5.6.36-log
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `employee`
+--
+
+DROP TABLE IF EXISTS `employee`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `employee` (
+  `Id` int(11) NOT NULL AUTO_INCREMENT,
+  `Account` varchar(50) NOT NULL,
+  `Name` varchar(50) NOT NULL,
+  `Age` int(11) NOT NULL DEFAULT '0',
+  `Status` tinyint(4) NOT NULL DEFAULT '0',
+  `CreateDate` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`Id`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8 COMMENT='员工表';
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `employee`
+--
+
+LOCK TABLES `employee` WRITE;
+/*!40000 ALTER TABLE `employee` DISABLE KEYS */;
+INSERT INTO `employee` VALUES (1,'songjiang','宋江',50,20,'2017-11-02 15:47:27'),(2,'lujunyi','卢俊义',48,20,'2017-11-02 15:47:50'),(3,'wuyong','吴用',46,10,'2017-11-02 16:07:36'),(4,'gongsunsheng','公孙胜',44,10,'2017-11-02 16:07:36'),(5,'guansheng','关胜',42,0,'2017-11-02 16:07:36'),(6,'linchōng','林冲',40,0,'2017-11-02 16:07:36');
+/*!40000 ALTER TABLE `employee` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping routines for database 'test'
+--
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2017-11-02 17:57:06
---
+++ UnitTest/UnitTest.cs
@@ -0,0 +1,394 @@
+using Dapper;
+using Dapper.Sugar;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using Microsoft.Extensions.Configuration;
+
+namespace UnitTest
+{
+    [TestClass]
+    public class UnitTest
+    {
+        List<EmployeeModel> list = new List<EmployeeModel>()
+        {
+            new EmployeeModel
+            {
+                Account = "qinming",
+                Name = "秦明",
+                Age = 36,
+                Status = EmployeeModel.EnumStatus.Complate,
+            },
+            new EmployeeModel
+            {
+                Account = "huyanzhuo",
+                Name = "呼延灼",
+                Age = 36,
+                Status = EmployeeModel.EnumStatus.Complate,
+            },
+            new EmployeeModel
+            {
+                Account = "huarong",
+                Name = "花荣",
+                Age = 34,
+                Status = EmployeeModel.EnumStatus.Complate,
+            },
+            new EmployeeModel
+            {
+                Account = "caijin",
+                Name = "柴进",
+                Age = 34,
+                Status = EmployeeModel.EnumStatus.Complate,
+            },
+            new EmployeeModel
+            {
+                Account = "liying",
+                Name = "李应",
+                Age = 34,
+                Status = EmployeeModel.EnumStatus.Complate,
+            },
+            new EmployeeModel
+            {
+                Account = "zhutong",
+                Name = "朱仝",
+                Age = 34,
+                Status = EmployeeModel.EnumStatus.Complate,
+            },
+            new EmployeeModel
+            {
+                Account = "luzhishen",
+                Name = "鲁智深",
+                Age = 34,
+                Status = EmployeeModel.EnumStatus.Complate,
+            },
+            new EmployeeModel
+            {
+                Account = "wusong",
+                Name = "武松",
+                Age = 34,
+                Status = EmployeeModel.EnumStatus.Complate,
+            }
+        };
+
+        #region 操作
+
+
+        /// <summary>
+        /// 新增-表名-单个匿名对象
+        /// </summary>
+        [TestMethod]
+        public void TestAdd1()
+        {
+            var result = DbHelp.ExecuteSql("employee", new
+            {
+                /*Id = 7,*/
+                Account = "qinming",
+                Name = "秦明",
+                Age = 38,
+                Status = 20
+            }, SugarCommandType.AddTableDirect);
+
+            Assert.AreEqual(result, 1, "新增-表名-单个匿名对象");
+        }
+
+        /// <summary>
+        /// 新增-表名-多个匿名对象
+        /// </summary>
+        [TestMethod]
+        public void TestAdd2()
+        {
+            var result = DbHelp.ExecuteSql("employee", list.Skip(1).Take(2).Select(t => new
+            {
+                Account = t.Account,
+                Name = t.Name,
+                Age = t.Age,
+                Status = t.Status
+            }).ToList(), SugarCommandType.AddTableDirect);
+
+            Assert.AreEqual(result, 2, "增-表名-多个匿名对象");
+        }
+
+        /// <summary>
+        /// 新增-表名-单个实体对象
+        /// </summary>
+        [TestMethod]
+        public void TestAdd3()
+        {
+            var result = DbHelp.ExecuteSql("employee", list.Skip(3).Take(1).First(), SugarCommandType.AddTableDirect);
+
+            Assert.AreEqual(result, 1, "新增-表名-单个实体对象");
+        }
+
+        /// <summary>
+        /// 新增-表名-多个个实体对象
+        /// </summary>
+        [TestMethod]
+        public void TestAdd4()
+        {
+            var result = DbHelp.ExecuteSql("employee", list.Skip(4).Take(2).ToList(), SugarCommandType.AddTableDirect);
+
+            Assert.AreEqual(result, 2, "新增-表名-多个个实体对象");
+        }
+
+
+        /// <summary>
+        /// 修改-表名-匿名对象
+        /// </summary>
+        [TestMethod]
+        public void TestUpdate1()
+        {
+            using (var conn = DbHelp.DbProvider.CreateConnection())
+            {
+                var info = DbHelp.DbProvider.QuerySingle<EmployeeModel>(conn, "employee", new { sq_Id = "Id = 1" }, SugarCommandType.QueryTableDirect);
+
+                Assert.IsNotNull(info, "查询单个对象");
+
+                var result = DbHelp.DbProvider.ExecuteSql(conn, "employee", new
+                {
+                    Id = 1,
+                    Account = "songjiang",
+                    Name = "宋江",
+                    sq_Age = "Age + 2",
+                    Status = 20
+                }, SugarCommandType.UpdateTableDirect);
+
+                Assert.AreEqual(result, 1, "修改-表名-匿名对象");
+
+                var info2 = DbHelp.DbProvider.QuerySingle<EmployeeModel>(conn, "employee", new
+                {
+                    sq_Id = "Id = 1"
+                }, SugarCommandType.QueryTableDirect);
+
+                Assert.IsNotNull(info2, "查询单个对象");
+
+                Assert.AreEqual(info2.Age, info.Age + 2, "修改-表名-匿名对象");
+            }
+        }
+
+        /// <summary>
+        /// 修改-表名-多个实体对象
+        /// </summary>
+        [TestMethod]
+        public void TestUpdate2()
+        {
+            var result = DbHelp.ExecuteSql("employee", new List<EmployeeModel>
+            {
+                new EmployeeModel
+                {
+                    Id = 1,
+                    Account = "songjiang",
+                    Name = "宋江",
+                    Age = 50,
+                    Status = EmployeeModel.EnumStatus.Complate
+                },
+                new EmployeeModel
+                {
+                    Id = 2,
+                    Account = "lujunyi",
+                    Name = "卢俊义",
+                    Age = 48,
+                    Status = EmployeeModel.EnumStatus.Disable
+                }
+            }, SugarCommandType.UpdateTableDirect);
+
+            Assert.AreEqual(result, 2, "修改-表名-多个实体对象");
+
+        }
+
+        #endregion
+
+        #region 查询
+
+        /// <summary>
+        /// 查询单个对象
+        /// </summary>
+        [TestMethod]
+        public void TestQuery1()
+        {
+            //查询单个对象
+            var result = DbHelp.QuerySingle<EmployeeModel>("employee", new
+            {
+                ig_Account = "songjiang",
+                sq_Account = "Account=@ig_Account",
+                Name_lk = "宋%",
+                Status = new int[] { 20 },
+                Age_gt = 40,
+                Age_lt = 52
+            }, SugarCommandType.QueryTableDirect);
+
+            Assert.IsNotNull(result, "查询单个对象");
+
+            Assert.AreEqual(result.Id, 1, "查询单个对象");
+        }
+
+        /// <summary>
+        /// 查询多个对象
+        /// </summary>
+        [TestMethod]
+        public void TestQuery2()
+        {
+            //查询多个对象
+            var result = DbHelp.QueryList<EmployeeModel>("employee", new { Age_ge = 50, Age_le = 50 }, SugarCommandType.QueryTableDirect);
+
+            Assert.AreEqual(result.Count, 1, "查询多个对象");
+        }
+
+        /// <summary>
+        /// 别名查询多个对象
+        /// </summary>
+        [TestMethod]
+        public void TestQuery3()
+        {
+            //别名查询多个对象
+            //new { ue_e_Id = 1, ge_e_Age = 48 }
+            dynamic param = new { e_Id_ue = 1, e_Age_ge = 48 };
+            //dynamic param = new ExpandoObject();
+            //param.e_Age_ge = 48;
+            //param.ue_e_Id = 1;
+            //param.ge_e_Age = 48;
+            var result = DbHelp.QueryList<EmployeeModel>("select * from employee e where", param, SugarCommandType.QuerySelectSql);
+
+            Assert.AreEqual(result.Count, 1, "别名查询多个对象错误");
+
+        }
+
+        /// <summary>
+        /// 带条件查询多个对象
+        /// </summary>
+        [TestMethod]
+        public void TestQuery4()
+        {
+            //带条件查询多个对象
+            var result = DbHelp.QueryList<EmployeeModel>("select * from employee e where e.Status=20 and", new { e_Age_ge = 48 }, SugarCommandType.QuerySelectSql);
+
+            Assert.AreEqual(result.Count, 1, "带条件查询多个对象错误");
+
+            Assert.AreEqual(result[0].Id, 1, "带条件查询多个对象错误");
+        }
+
+        /// <summary>
+        /// 查询个数
+        /// </summary>
+        [TestMethod]
+        public void TestQuery5()
+        {
+            //别名查询多个对象
+            //new { ue_e_Id = 1, ge_e_Age = 48 }
+            dynamic param = new { e_Id_ue = 1, e_Age_ge = 48 };
+            //dynamic param = new ExpandoObject();
+            //param.ue_e_Id = 1;
+            //param.ge_e_Age = 48;
+            int result = 0;
+            List<EmployeeModel> result2 = null;
+            using (var conn = DbHelp.DbProvider.CreateConnection())
+            {
+                result = DbHelp.DbProvider.QueryScalar<int>(conn, "select Count(*) from employee e where", param, SugarCommandType.QuerySelectSql);
+
+                result2 = DbHelp.DbProvider.QueryList<EmployeeModel>(conn, "select * from employee e where e.Status=20 and", new { e_Age_ge = 48 }, SugarCommandType.QuerySelectSql);
+            }
+
+            Assert.AreEqual(result, 1, "查询个数错误");
+
+            Assert.AreEqual(result2.Count, 1, "带条件查询多个对象错误");
+
+            Assert.AreEqual(result2[0].Id, 1, "带条件查询多个对象错误");
+
+        }
+
+        /// <summary>
+        /// 分页查询多个对象 - 内存分页
+        /// </summary>
+        [TestMethod]
+        public void TestQueryPaging1()
+        {
+            //带条件查询多个对象
+            var result = DbHelp.QueryPagingList<EmployeeModel>(1, 2, "select * from employee e where e.Status>0 and", new { e_Id_ue = 1 }, SugarCommandType.QuerySelectSql);
+
+            Assert.AreEqual(result.List.Count, 2, "带条件查询多个对象错误");
+
+            Assert.AreEqual(result.List[0].Id, 4, "带条件查询多个对象错误");
+
+        }
+
+        /// <summary>
+        /// 分页查询多个对象 - limit分页
+        /// </summary>
+        [TestMethod]
+        public void TestQueryPaging2()
+        {
+            //带条件查询多个对象
+            var result = DbHelp.QueryPagingList2<EmployeeModel>(1, 2, "select * from employee e where e.Status>0 and", new { e_Id_ue = 1 }, SugarCommandType.QuerySelectSql);
+
+            Assert.AreEqual(result.List.Count, 2, "带条件查询多个对象错误");
+
+            Assert.AreEqual(result.List[0].Id, 4, "带条件查询多个对象错误");
+
+        }
+
+        #endregion
+
+        #region 存储过程
+
+        /// <summary>
+        /// 调用存储过程
+        /// </summary>
+        [TestMethod]
+        public void TestStoredProcedure1()
+        {
+            //存储过程，根据存储名称调用存储过程
+            var p = new DynamicParameters();
+            p.Add("@startId", 6);
+
+            var result = DbHelp.ExecuteSql("delete_data", p, SugarCommandType.StoredProcedure);
+
+            Assert.AreEqual(result, 6, "调用存储过程错误");
+        }
+
+        /// <summary>
+        /// 调用存储过程
+        /// </summary>
+        [TestMethod]
+        public void TestStoredProcedure2()
+        {
+            //存储过程，根据存储名称调用存储过程
+            var result = DbHelp.ExecuteSql("delete_data", new { startId = 6 }, SugarCommandType.StoredProcedure);
+
+            Assert.AreEqual(result, 0, "调用存储过程错误");
+        }
+
+        #endregion
+    }
+
+    public class EmployeeModel
+    {
+        public int Id { get; set; }
+        public string Account { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public EnumStatus Status { get; set; }
+        [IgnoreAdd, IgnoreUpdate]
+        public DateTime CreateDate { get; set; }
+
+        public enum EnumStatus
+        {
+            /// <summary>
+            /// 删除
+            /// </summary>
+            Delete = 0,
+
+            /// <summary>
+            /// 禁用
+            /// </summary>
+            Disable = 10,
+
+            /// <summary>
+            /// 成功
+            /// </summary>
+            Complate = 20
+        }
+    }
+}
